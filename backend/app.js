faee54369d8e5976639463762b3b32c983182326
akshpawar2003 <akashpawar111213@gmail.com>
2025-11-08 14:45:22 +0530

    update backend folder deleteController

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,7 @@ const app = express();
 app.use(express.json());
 const PORT = 3000
 
-export let tasksArray = [
-  { id: 1, title: "Home", descriptions: "I have to clean Home " },
-  { id: 2, title: "Office", descriptions: "I Have completing project" },
-]; 
-
 app.use("/tasks",taskRouter)
-console.log(tasksArray);
 
 app.listen(PORT,()=>{
   console.log(`Server is Listening http://localhost:${PORT}`)
--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { tasksArray } from "../app.js";
 
+let tasksArray = [
+  { id: 1, title: "Home", descriptions: "I have to clean Home " },
+  { id: 2, title: "Office", descriptions: "I Have completing project" },
+]; 
 //Retrieve a list of all tasks
 export const allTask = (req,res) => {
   try {
@@ -67,7 +70,11 @@ export const updateTask = (req,res) => {
 
     task.title = req.body.title;
     task.descriptions = req.body.descriptions;
-    res.json(task);
+    res.status(200).json({
+            success: true,
+            message: 'Task Update SuccesFully',
+            tasksArray
+        })
   } catch (error) {
     res.status(400).json({
             success: false,
@@ -76,22 +83,28 @@ export const updateTask = (req,res) => {
   }
 }
 
-//Delete a task by ID. 
-export const deleteTask = (req,res) => {
+//Delete a task by ID
+export const deleteTask = (req, res) => {
   try {
-    const id = req.params.id
-    if(!id){
-      return res.status(404).json({success: false,message: 'This Task not found'})
+    const id = parseInt(req.params.id);
+    if (!id) {
+      return res.status(404).json({
+        success: false,
+        message: "Task ID is required",
+      });
     }
-    const deleteFromArray = tasksArray.filter(task => id !== task.id)
+    tasksArray = tasksArray.filter(task => task.id !== id);
+
     res.status(200).json({
-            success: true,
-            message: 'Task deleted successfully'
-        });
+      success: true,
+      message: "Task deleted successfully",
+      tasksArray,
+    });
+
   } catch (error) {
     res.status(400).json({
-            success: false,
-            message: error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
-}
+};
